5b4dae93e236886c930a1410e3e4829eb2fafc37
achilleleal <achilleleal@gmail.com>
2020-10-12 15:11:24 -0400

    set up editor component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Editor from './Editor'
 
 function App() {
-  return (
-  <div>
 
-  </div>
+  const [html, setHtml] = useState('')
+  const [css, setCss] = useState('')
+  const [js, setJs] = useState('')
+
+  return (
+    <>
+      <div className="pane top-pane">
+        <Editor 
+          lang="xml"
+          title="HTML" 
+          value={html} 
+          onChange={setHtml}
+        />
+        <Editor 
+          lang="javascript" 
+          title="JS" 
+          value={css} 
+          onChange={setCss}
+        />
+        <Editor 
+          lang="css" 
+          title="CSS" 
+          value={js} 
+          onChange={setJs}
+        />
+      </div>
+      <div className="pane">
+        <iframe
+          title="output"
+          sandbox="allow-scripts"
+          frameBorder="0"
+          width="100%"
+          height="100%"
+        />
+      </div>
+    </>
   );
 }
 
---
+++ src/components/Editor.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import 'codemirror/lib/codemirror.css'
+import 'codemirror/theme/material.css'
+import 'codemirror/mode/xml/xml'
+import 'codemirror/mode/javascript/javascript'
+import 'codemirror/mode/css/css'
+import { Controlled as ControlledEditor } from 'react-codemirror2'
+
+export default function Editor({ title, lang, value, onChange }) {
+
+    const handleChange = (editor, data, value) => onChange(value);
+
+    return (
+        <div className="editor-container">
+            <div className="editor-title">
+                {title}
+                <button>O/C</button>
+            </div>
+            <ControlledEditor 
+              onBeforeChange={handleChange}
+              value={value}
+              className="code-mirror-wrapper"
+              options={{
+                  lineWrapping: true,
+                  lint: true,
+                  mode: lang,
+                  lineNumbers: true,
+                  theme: 'material'
+              }}/>
+        </div>
+    )
+}
